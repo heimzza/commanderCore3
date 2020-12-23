2eee49a6e6b01e47c0aca99135d188cdcb264cb9
heimzza <hamzassm18@gmail.com>
2020-12-23 23:54:24 +0300

    first commit

---
+++ Controllers/CommandsController.cs
@@ -0,0 +1,40 @@
+using System.Collections.Generic;
+using Commander.Data;
+using Commander.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Commander.Controllers
+{
+    //api/commands
+    [Route("api/commands")]
+    [ApiController]
+    public class CommandsController : ControllerBase
+    {
+        private readonly ICommanderRepo _repository;
+
+        public CommandsController(ICommanderRepo repository)
+        {
+            _repository = repository;
+        }
+
+        //private readonly MockCommanderRepo _repository = new MockCommanderRepo();
+        //GET api/commands
+        [HttpGet]
+        public ActionResult<IEnumerable<Command>> GetAllCommands()
+        {
+            var commandItems = _repository.GetAppCommands();
+
+            return Ok(commandItems);
+        }
+
+        //GET api/commands/{id}
+        [HttpGet("{id}")]
+        public ActionResult<Command> GetCommandById(int id)
+        {
+            var commandItem = _repository.GetCommandById(id);
+
+            return Ok(commandItem);
+        }
+
+    }
+}
---
+++ Data/MockCommanderRepo.cs
@@ -0,0 +1,26 @@
+using System.Collections.Generic;
+using Commander.Models;
+
+namespace Commander.Data
+{
+    public class MockCommanderRepo : ICommanderRepo
+    {
+        public IEnumerable<Command> GetAppCommands()
+        {
+            var commands = new List<Command>
+            {
+                new Command { Id = 0, HowTo = "Make sandwich", Line = "Cut Bread", Platform = "Desk"},
+                new Command { Id = 1, HowTo = "Cut bread", Line = "Get a knife", Platform = "knide & chopping board"},
+                new Command { Id = 2, HowTo = "Make cup of tea", Line = "Place teabag in cup", Platform = "Kettle & cup"}
+
+            };
+
+            return commands;
+        }
+
+        public Command GetCommandById(int id)
+        {
+            return new Command { Id = 0, HowTo = "Make sandwich", Line = "Cut Bread", Platform = "Desk" };
+        }
+    }
+}
