a57db014ac49e6c0af390cc268bbd9265dcd93b5
heimzza <hamzassm18@gmail.com>
2020-12-24 14:51:29 +0300

    entity framework and db connections

--- Data/CommanderContext.cs
+++ Data/CommanderContext.cs
@@ -1,9 +1,18 @@
+using Commander.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace Commander.Data
 {
     public class CommanderContext : DbContext
     {
+        public CommanderContext(DbContextOptions<CommanderContext> opt) : base(opt)
+        {
+
+        }
+
+        public DbSet<Command> Commands { get; set; }
+
+
 
     }
 }
